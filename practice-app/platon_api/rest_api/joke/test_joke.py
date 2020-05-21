bded5cc269d4864bf6b6acc14176f2206a9cf5a8
ertbulbull <ertbulbull@gmail.com>
2020-05-22 02:08:07 +0300

    Test cases added.

---
+++ practice-app/platon_api/rest_api/joke/test_joke.py
@@ -0,0 +1,23 @@
+from django.test import testcases
+import requests
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.response import Response
+
+class JokeTest(APITestCase):
+
+
+    def test_valid_token(self):
+        data = { "token" : "03684b7c7efc7ba7ffe53f32f3c9346d8383fcc13eddd1272705c746ba986f2f"}
+        response = self.client.request("/api/joke" ,data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_invalid_token(self):
+        data = {"token": "invalid"}
+        response = self.client.request("/api/joke", data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_without_token(self):
+        data = {}
+        response = self.client.request("/api/joke", data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
