c8ad7c84cd9ba4da6e572cb4ba661b6fa3d54caa
Mehmet Temizel <mehmet.temizel@boun.edu.tr>
2020-05-23 19:25:52 +0300

    Initial Django Project

--- practice-app/platon_api/rest_api/views.py
+++ practice-app/platon_api/rest_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+#from rest_api.forgot_password import forgot_password
+
+#def forgot_password(request):
+ #     return forgot_password(request)
+ if request.GET.get("token"):
+    token = request.GET["token"]
+
+
 
 # Create your views here.
