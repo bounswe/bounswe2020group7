9b66f8b702fe186d937b8bf255d2ef166e10797b
Mehmet Temizel <mehmet.temizel@boun.edu.tr>
2020-05-23 19:24:13 +0300

    Initial Django Project

--- practice-app/platon_api/platon_api/urls.py
+++ practice-app/platon_api/platon_api/urls.py
@@ -1,7 +1,7 @@
 """platon_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_api import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   # path('api/forgot_password/', views.forgot_password, name="forgot_password"),
+    path('account/', include('django.contrib.auth.urls')),
 ]
