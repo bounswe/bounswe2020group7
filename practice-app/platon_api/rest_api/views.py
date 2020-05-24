fc0ba410e5f70270c5f8728074fbf847659abd72
ertbulbull <ertbulbull@gmail.com>
2020-05-24 18:49:38 +0300

    Views.py updated.

--- practice-app/platon_api/rest_api/views.py
+++ practice-app/platon_api/rest_api/views.py
@@ -1,10 +1,6 @@
 from rest_api.joke.joke import joke_api
 from rest_framework.decorators import api_view
 
-def joke(response):
-    return joke_api(response)
-
-
 @api_view(["GET"])
 def joke(request):
     return joke_api(request)
