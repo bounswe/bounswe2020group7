aa8b0d37a1c8344c9278ea860ba3c767cbcc790e
ertbulbul <44380647+ertbulbul@users.noreply.github.com>
2020-05-25 02:07:12 +0300

    Update settings.py

--- practice-app/platon_api/platon_api/settings.py
+++ practice-app/platon_api/platon_api/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
