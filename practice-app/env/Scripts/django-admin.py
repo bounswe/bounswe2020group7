27921b1bf6ec579001cdb3c3b8c74799ce2f8408
burak <burak.omur.1998@gmail.com>
2020-05-25 15:38:54 +0300

    virtual env and some cache deleted

--- practice-app/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Meltem Arslan\django\mysite2\bounswe2020group7\practice-app\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
