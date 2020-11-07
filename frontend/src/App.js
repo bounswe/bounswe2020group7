82f2fb563a2290a939ed1144740cf610aa1317fb
ahmetdadak <ahmetdadak06@gmail.com>
2020-11-08 00:44:35 +0300

    react-app is created

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo_icon from './logo/platon_logo_icon_orijinal.svg';
+import logo from './logo/platon_logo_orijinal.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo_icon} className="App-logo-icon" alt="logo_icon" />
+
+
+        <div className="Coming-Soon">
+          Coming Soon
+        </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
