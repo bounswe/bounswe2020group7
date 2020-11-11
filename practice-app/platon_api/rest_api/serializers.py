bc6be05e8df9f4646c57b153465fdb3b2622f6f9
Burak Ömür <burak.omur.1998@gmail.com>
2020-11-11 18:10:01 +0300

    clean repo

--- practice-app/platon_api/rest_api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import RegisteredUser
-
-class RegisteredUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RegisteredUser
-        fields = '__all__'
