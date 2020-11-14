7783480276c5d3c91a4ba09289937fab41767e1d
ahmetdadak <ahmetdadak06@gmail.com>
2020-11-14 19:11:36 +0300

    appbar optimization

--- frontend/src/components/AppBar/AppBar.js
+++ frontend/src/components/AppBar/AppBar.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
-import LogoIcon from '../../Logo/LogoIcon';
 import Logo from '../../Logo/Logo';
 import colors from '../../utils/colors';
 const useStyles = makeStyles((theme) => ({
