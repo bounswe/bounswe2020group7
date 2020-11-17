c5137d2a91e33e0f2b0f1caf8c50415c5910fdb3
Halil Umut Özdemir <halil.ozdemir@boun.edu.tr>
2020-11-17 18:35:09 +0300

    Research information tests are implemented

--- Platon/backend/app/profile_management/views.py
+++ Platon/backend/app/profile_management/views.py
@@ -1,6 +1,13 @@
 from flask import Response
 from flask_restplus import Resource
+from enum import Enum
 
+class ResearchType(Enum):
+    HAND_WRITTEN = 0
+    RESEARCHGATE = 1
+    GOOGLE_SCHOLAR = 2
+    BOTH_RG_AND_GS = 3
+    
 class ProfileAPI(Resource):
     def get(self):
         return Response("Platon API is under development!!",200)
