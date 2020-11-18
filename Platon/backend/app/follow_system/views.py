e2791d281b00258110ec39f2a883fa6945c523e4
can <canbolukbas98@gmail.com>
2020-11-18 23:08:40 +0300

    Daily Improvement
    - Test cases are completed.
    - All test cases are passed.
    - Login_Required will be added.
    - Private Account Protection will be added.
    - Documentation will be added.

--- Platon/backend/app/follow_system/views.py
+++ Platon/backend/app/follow_system/views.py
@@ -1,36 +1,32 @@
-from flask import make_response,jsonify,request
+from flask import make_response, jsonify, request
 from flask_restplus import Resource
 from flask import current_app as app
 
 from app import api, db
-from app.follow_system.forms import GetFollowingsForm, GetFollowersForm, GetFollowRequestsForm, SendFollowRequestsForm, AcceptFollowRequestsForm, RejectFollowRequestsForm
+from app.follow_system.forms import GetFollowingsForm, GetFollowersForm, GetFollowRequestsForm, \
+    SendFollowRequestsForm, AcceptFollowRequestsForm, RejectFollowRequestsForm
 from app.follow_system.models import Follow, FollowRequests
 
-
-#import jwt
 import datetime
 from dateutil import parser
 from hashlib import sha256
 from functools import wraps
 
 
-#Input: follower_id
-#Output: list containing following_id values.
+# Private Account Restrictions not implemented.
 class GetFollowingsAPI(Resource):
 
     def post(self):
-        print("GetFollowingsAPI's post")
         form = GetFollowingsForm(request.form)
         if form.validate():
             try:
                 followSearch = Follow.query.filter(Follow.follower_id == form.follower_id.data).all()
             except:
-                return make_response(jsonify({'error' : 'Database Connection Problem'}),500)
-            '''
+                return make_response(jsonify({'error': 'Database Connection Problem'}), 500)
+
             if followSearch is []:
-                return make_response(jsonify({'error' : 'Followings Not Found'}),404)
-            '''
-            #print(followSearch)
+                return make_response(jsonify({'error': 'No User Found'}), 404)
+
             mylist = []
             for follow in followSearch:
                 mylist.append(follow.following_id)
@@ -38,23 +34,23 @@ class GetFollowingsAPI(Resource):
             return make_response(jsonify(mylist), 200)
 
         else:
-            return make_response(jsonify({'error': ''}), 401)
+            return make_response(jsonify({'error': 'Bad Request'}), 400)
 
+
+# Private Account Restrictions not implemented.
 class GetFollowersAPI(Resource):
 
     def post(self):
-        print("GetFollowersAPI's post")
         form = GetFollowersForm(request.form)
         if form.validate():
             try:
                 followSearch = Follow.query.filter(Follow.following_id == form.following_id.data).all()
             except:
                 return make_response(jsonify({'error': 'Database Connection Problem'}), 500)
-            '''
+
             if followSearch is []:
-                return make_response(jsonify({'error' : 'Followings Not Found'}),404)
-            '''
-            # print(followSearch)
+                return make_response(jsonify({'error': 'No User Found'}), 404)
+
             mylist = []
             for follow in followSearch:
                 mylist.append(follow.follower_id)
@@ -62,23 +58,22 @@ class GetFollowersAPI(Resource):
             return make_response(jsonify(mylist), 200)
 
         else:
-            return make_response(jsonify({'error': ''}), 401)
+            return make_response(jsonify({'error': 'Bad Request'}), 400)
 
+# login_required decorator will be added.
 class GetFollowRequestsAPI(Resource):
 
     def post(self):
-        print("GetFollowRequestsAPI's post")
         form = GetFollowRequestsForm(request.form)
         if form.validate():
             try:
                 followSearch = FollowRequests.query.filter(FollowRequests.following_id == form.following_id.data).all()
             except:
                 return make_response(jsonify({'error': 'Database Connection Problem'}), 500)
-            '''
+
             if followSearch is []:
-                return make_response(jsonify({'error' : 'Followings Not Found'}),404)
-            '''
-            # print(followSearch)
+                return make_response(jsonify({'error': 'No Follow Request Found'}), 404)
+
             mylist = []
             for follow in followSearch:
                 mylist.append(follow.follower_id)
@@ -86,71 +81,96 @@ class GetFollowRequestsAPI(Resource):
             return make_response(jsonify(mylist), 200)
 
         else:
-            return make_response(jsonify({'error': ''}), 401)
+            return make_response(jsonify({'error': 'Bad Request'}), 400)
 
+
+# login_required decorator will be added.
 class SendFollowRequestAPI(Resource):
 
     def post(self):
-        print("SendFollowRequestAPI's post")
         form = SendFollowRequestsForm(request.form)
         if form.validate():
+            fr = FollowRequests(form.follower_id.data, form.following_id.data)
             try:
-                fr = FollowRequests(form.follower_id.data, form.following_id.data)
                 db.session.add(fr)  # Creating a new database entry.
                 db.session.commit()
             except:
                 return make_response(jsonify({'error': 'Database Connection Problem'}), 500)
 
-            return make_response(jsonify({'msg' : 'Follow Request is succesfully added'}),200)
+            return make_response(jsonify({'msg': 'Follow Request is succesfully added'}), 200)
 
         else:
-            return make_response(jsonify({'error': ''}), 401)
+            return make_response(jsonify({'error': 'Bad Request'}), 400)
 
-'''
+
+# login_required decorator will be added.
 class AcceptFollowRequestAPI(Resource):
 
     def post(self):
-        print("AcceptFollowRequestAPI's post")
         form = AcceptFollowRequestsForm(request.form)
         if form.validate():
 
             # Check if the given FollowRequest instance exists.
             try:
-                followSearch = FollowRequests.query.filter(FollowRequests.following_id == form.following_id.data and ).all()
+                followSearch = FollowRequests.query.filter(FollowRequests.following_id == form.following_id.data,
+                                                           FollowRequests.follower_id == form.follower_id.data).all()
             except:
                 return make_response(jsonify({'error': 'Database Connection Problem'}), 500)
-            # If not, return error.
+
+            if len(followSearch)==0:
+                return make_response(jsonify({'error': 'No FollowRequest found'}), 404)
+
             # If it exists: Create a Follow instance. Delete FollowRequest instance.
-
-
-
+            follow_request = followSearch[0]
+            new_follow_entry = Follow(follow_request.follower_id, follow_request.following_id)
             try:
-                fr = FollowRequests(form.follower_id.data, form.following_id.data)
-                db.session.add(fr)  # Creating a new database entry.
+                db.session.add(new_follow_entry)
+                db.session.delete(follow_request)
                 db.session.commit()
             except:
                 return make_response(jsonify({'error': 'Database Connection Problem'}), 500)
 
-            return make_response(jsonify({'msg' : 'Follow Request is succesfully accepted'}),200)
+            return make_response(jsonify({'msg': 'Follow Request is succesfully accepted'}), 200)
 
         else:
-            return make_response(jsonify({'error': ''}), 401)
+            return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
+# login_required decorator will be added.
 class RejectFollowRequestAPI(Resource):
 
     def post(self):
-        form = RejectFollowRequestsForm(request.form)
-        print("hi")
-        
-'''
+        form = AcceptFollowRequestsForm(request.form)
+        if form.validate():
+
+            # Check if the given FollowRequest instance exists.
+            try:
+                followSearch = FollowRequests.query.filter(FollowRequests.following_id == form.following_id.data,
+                                                           FollowRequests.follower_id == form.follower_id.data).all()
+            except:
+                return make_response(jsonify({'error': 'Database Connection Problem'}), 500)
+
+            if len(followSearch) == 0:
+                return make_response(jsonify({'error': 'No FollowRequest found'}), 404)
+
+            # If it exists: Delete FollowRequest instance.
+            follow_request = followSearch[0]
+            try:
+                db.session.delete(follow_request)
+                db.session.commit()
+            except:
+                return make_response(jsonify({'error': 'Database Connection Problem'}), 500)
+
+            return make_response(jsonify({'msg': 'Follow Request is succesfully accepted'}), 200)
+
+        else:
+            return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 def register_resources(api):
-    api.add_resource(GetFollowingsAPI,"/follow/followings")
+    api.add_resource(GetFollowingsAPI, "/follow/followings")
     api.add_resource(GetFollowersAPI, "/follow/followers")
     api.add_resource(GetFollowRequestsAPI, "/follow/get_follow_requests")
     api.add_resource(SendFollowRequestAPI, "/follow/send_follow_requests")
-    #api.add_resource(AcceptFollowRequestAPI,"/follow/accept_follow_requests")
-    #api.add_resource(RejectFollowRequestAPI, "/follow/reject_follow_requests")
-
+    api.add_resource(AcceptFollowRequestAPI, "/follow/accept_follow_requests")
+    api.add_resource(RejectFollowRequestAPI, "/follow/reject_follow_requests")
