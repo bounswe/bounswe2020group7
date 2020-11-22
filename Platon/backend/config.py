3b11b5524642da1d61e8cd46b9d701ecfcc76f00
Halil Umut Özdemir <halil.ozdemir@boun.edu.tr>
2020-11-22 12:40:01 +0300

    HOST NAME added to configuration

--- Platon/backend/config.py
+++ Platon/backend/config.py
@@ -4,7 +4,9 @@ DEBUG = True
 
 # Define the application directory
 import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+FRONTEND_HOSTNAME = "ec2-52-59-254-130.eu-central-1.compute.amazonaws.com/"
 
 # Define the database - we are working with
 mysql_user = os.getenv('MYSQL_USER')
