7c23f175cbc3bf38732be050317b74471cea8796
Halil Umut Özdemir <halil.ozdemir@boun.edu.tr>
2020-11-22 12:19:51 +0300

    Small Bug Fix

--- Platon/backend/test.py
+++ Platon/backend/test.py
@@ -5,8 +5,8 @@ from tests.profile_management_test import ResearchInfoTests,NotificationTests
 import sqlalchemy
 
 if __name__ == "__main__":
-    engine = sqlalchemy.create_engine("mysql+pymysql://root:rootpassword@52.59.254.130:3306") # connect to server
-    engine.execute("DROP SCHEMA IF EXISTS platondb_test;")
-    engine.execute("CREATE SCHEMA IF NOT EXISTS `platondb_test`;") #create db
+    engine = sqlalchemy.create_engine("mysql+pymysql://root:rootpassword@52.59.254.130:3306") 
+    engine.execute("DROP SCHEMA IF EXISTS `platondb_test`;")
+    engine.execute("CREATE SCHEMA IF NOT EXISTS `platondb_test`;") 
     unittest.main()
     engine.execute("DROP SCHEMA IF EXISTS platondb_test;")
