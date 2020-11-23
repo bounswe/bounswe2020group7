2d7784f571a5b70936b1f08a185494154bfde5b9
Ertugrul <ertugrul@codeway.co>
2020-11-24 02:02:56 +0300

    Activity stream added.

--- Platon/app/src/main/java/com/cmpe451/platon/page/fragment/home/presenter/HomePresenter.kt
+++ Platon/app/src/main/java/com/cmpe451/platon/page/fragment/home/presenter/HomePresenter.kt
@@ -9,6 +9,7 @@ import com.cmpe451.platon.page.activity.LoginActivity
 import com.cmpe451.platon.page.fragment.home.contract.HomeContract
 import com.cmpe451.platon.page.fragment.home.model.HomeRepository
 import com.cmpe451.platon.page.fragment.home.view.HomeFragment
+import com.cmpe451.platon.util.Definitions
 
 class HomePresenter(private var view:HomeContract.View?, private var repository: HomeRepository, private var sharedPreferences: SharedPreferences, private var navController: NavController) : HomeContract.Presenter {
 
@@ -22,5 +23,17 @@ class HomePresenter(private var view:HomeContract.View?, private var repository:
 
     override fun onDestroy() {
         this.view = null
+    }
+
+    override fun getUpcomingEvents(): ArrayList<Definitions.UpcomingEvent> {
+        return repository.fetchUpcomingEvents((view as HomeFragment).activity)
+    }
+
+    override fun getTrendingProjects(): ArrayList<Definitions.TrendingProject> {
+        return  repository.fetchTrendingProjects((view as HomeFragment).activity)
+    }
+
+    override fun getActivities(): ArrayList<Definitions.ActivityStream> {
+        return repository.fetchActivityStream((view as HomeFragment).activity)
     }
 }
