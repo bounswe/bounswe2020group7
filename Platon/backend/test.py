7867908660c70d2b390a2a0a003eac5efa94a728
Halil Umut Özdemir <halil.ozdemir@boun.edu.tr>
2020-12-08 02:05:16 +0300

    CI and test DB updates

--- Platon/backend/test.py
+++ Platon/backend/test.py
@@ -1,11 +1,12 @@
 import unittest
-from tests.auth_system_test import LoginTest,ResetPasswordTest
-#from tests.follow_test import FollowTest
-from tests.profile_management_test import ResearchInfoTests,NotificationTests
+#from tests.auth_system_test import LoginTest,ResetPasswordTest
+from tests.follow_test import FollowTest
+#from tests.profile_management_test import ResearchInfoTests,NotificationTests
+from tests.base_test import TestConfig
 import sqlalchemy
 
 if __name__ == "__main__":
-    engine = sqlalchemy.create_engine("mysql+pymysql://root:rootpassword@52.59.254.130:3306") 
+    engine = sqlalchemy.create_engine("mysql+pymysql://{}:{}@{}:{}".format(TestConfig.mysql_user, TestConfig.mysql_password, TestConfig.mysql_host, TestConfig.mysql_port)) 
     engine.execute("DROP SCHEMA IF EXISTS `platondb_test`;")
     engine.execute("CREATE SCHEMA IF NOT EXISTS `platondb_test`;") 
     unittest.main()
