fbe6a3143a3da405ddc56b8ed3ce03128bae108d
Halil Umut Özdemir <halil.ozdemir@boun.edu.tr>
2020-12-11 00:47:58 +0300

    db problem fix

--- Platon/backend/app/models.py
+++ Platon/backend/app/models.py
@@ -1,4 +1,4 @@
 from app.auth_system import models
 from app.follow_system import models
 from app.profile_management import models
-from app.search_engine import moodels
+from app.search_engine import models
