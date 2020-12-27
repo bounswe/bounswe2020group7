1336b3446eb291baab79c0577bf815121aebeb2c
ahmetdadak <ahmetdadak06@gmail.com>
2020-12-27 13:25:01 +0300

    homepage replacement solved trend ws is linked

--- Platon/frontend/src/components/TrendingProjects/TrendingProjectsItem/TrendingProjectsItem.js
+++ Platon/frontend/src/components/TrendingProjects/TrendingProjectsItem/TrendingProjectsItem.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import { withStyles, makeStyles } from '@material-ui/core/styles'
-import MuiAccordion from '@material-ui/core/Accordion'
-import MuiAccordionSummary from '@material-ui/core/AccordionSummary'
-import MuiAccordionDetails from '@material-ui/core/AccordionDetails'
-import Typography from '@material-ui/core/Typography'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import colors from '../../../utils/colors'
-
+import React from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import MuiAccordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import colors from "../../../utils/colors";
+import { Link } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
+    width: "100%",
     maxWidth: 500,
     margin: 0,
     padding: 0,
@@ -22,94 +22,112 @@ const useStyles = makeStyles((theme) => ({
   secondary: {
     color: colors.primaryDark,
   },
-}))
+}));
 
 const Accordion = withStyles({
   root: {
-    width: '100%',
+    width: "100%",
     backgroundColor: colors.primary,
-    borderRadius: '0.5em',
-    '&:not(:last-child)': {
+    borderRadius: "0.5em",
+    "&:not(:last-child)": {
       borderBottom: 0,
     },
-    '&:before': {
-      display: 'none',
+    "&:before": {
+      display: "none",
     },
-    '&$expanded': {
-      margin: 'auto',
+    "&$expanded": {
+      margin: "auto",
     },
   },
   expanded: {},
-})(MuiAccordion)
+})(MuiAccordion);
 
 const AccordionSummary = withStyles({
   root: {
     backgroundColor: colors.tertiaryDark,
 
-    borderRadius: '0.5em',
+    borderRadius: "0.5em",
 
     minHeight: 56,
-    '&$expanded': {
+    "&$expanded": {
       minHeight: 56,
     },
   },
 
   content: {
-    '&$expanded': {
-      margin: '12px 0',
+    "&$expanded": {
+      margin: "12px 0",
     },
   },
   expanded: {},
-})(MuiAccordionSummary)
+})(MuiAccordionSummary);
 
 const AccordionDetails = withStyles((theme) => ({
   root: {
     color: colors.secondary,
     backgroundColor: colors.primary,
-    borderRadius: '0.5em',
+    borderRadius: "0.5em",
   },
-}))(MuiAccordionDetails)
+}))(MuiAccordionDetails);
 
-const TrendingProjectsItem = ({ id, title, description, contributors }) => {
-  const classes = useStyles()
-  const [expanded, setExpanded] = React.useState('')
+const TrendingProjectsItem = ({
+  id,
+  title,
+  description,
+  contributors,
+  profileId,
+  workspaceId,
+}) => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState("");
 
   const handleChange = (panel) => (event, newExpanded) => {
-    setExpanded(newExpanded ? panel : false)
-  }
-
+    setExpanded(newExpanded ? panel : false);
+  };
 
   const contributorsText = contributors.reduce((acc, contributor, index) => {
-    const nameSurname = `${contributor.name} ${contributor.surname}`
-    return index === 0 ? acc + nameSurname : acc + ` - ${nameSurname}`
-  }, '')
+    const nameSurname = `${contributor.name} ${contributor.surname}`;
+    return index === 0 ? acc + nameSurname : acc + ` - ${nameSurname}`;
+  }, "");
 
   return (
     <List className={classes.root}>
       <ListItem disableGutters>
         <Accordion
           square
-          expanded={expanded === 'panel' + id}
-          onChange={handleChange('panel' + id)}
+          expanded={expanded === "panel" + id}
+          onChange={handleChange("panel" + id)}
         >
           <AccordionSummary
-            aria-controls={'panel' + id + ' d-content '}
-            id={'panel' + id + ' d-header '}
+            aria-controls={"panel" + id + " d-content "}
+            id={"panel" + id + " d-header "}
           >
             <ListItemText
-              classes={{ primary: classes.primary, secondary: classes.secondary }}
-              primary={title} secondary={contributorsText}
+              classes={{
+                primary: classes.primary,
+                secondary: classes.secondary,
+              }}
+              primary={title}
+              secondary={contributorsText}
             />
           </AccordionSummary>
-          <AccordionDetails>
-            <Typography>
-              {description}
-            </Typography>
+          <AccordionDetails
+            style={{ display: "flex", flexDirection: "column" }}
+          >
+            <Typography>{description}</Typography>
+            {localStorage.getItem("jwtToken") ? (
+              <Link
+                to={`/${profileId}/workspace/${workspaceId}`}
+                style={{ textDecoration: "none" }}
+              >
+                See details
+              </Link>
+            ) : null}
           </AccordionDetails>
         </Accordion>
       </ListItem>
     </List>
-  )
-}
+  );
+};
 
-export default TrendingProjectsItem
+export default TrendingProjectsItem;
