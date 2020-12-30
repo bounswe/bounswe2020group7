1158f610345ca5d2741f94bb41b35f018c728cd2
ahmetdadak <ahmetdadak06@gmail.com>
2020-12-30 17:40:22 +0300

    upcoming event attaching added to workspace create

--- Platon/frontend/src/components/Workspace/WorkspaceInput/WorkspaceInputDate/WorkspaceInputDate.js
+++ Platon/frontend/src/components/Workspace/WorkspaceInput/WorkspaceInputDate/WorkspaceInputDate.js
@@ -4,6 +4,10 @@ import TextField from '@material-ui/core/TextField';
 import colors from '../../../../utils/colors';
 import FormControl from "@material-ui/core/FormControl";
 import FormLabel from "@material-ui/core/FormLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
+import { Menu } from '@material-ui/core';
 
 const StyledTextField = withStyles({
     root: {
@@ -33,12 +37,53 @@ const StyledTextField = withStyles({
       },
     },
   })(TextField);
+  const StyledFormControl = withStyles({
+    root: {
+
+      '& .MuiInputBase-input': {
+        color: colors.secondary,
+      },
+      "& .Mui-required": {
+        color: colors.primaryLight,
+      },
+      '& label.Mui-focused': {
+        color: colors.tertiary,
+      },
+      '& .MuiInput-underline:after': {
+        borderBottomColor: colors.tertiary,
+      },
+      '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+          borderColor: colors.secondaryLight,
+        },
+        '&:hover fieldset': {
+          borderColor: colors.secondaryDark,
+        },
+        '&.Mui-focused fieldset': {
+          borderColor: colors.tertiary,
+        },
+      },
+    },
+  })(FormControl);
+
+
+
+  const StyledMenuItem = withStyles({
+    root: {
+      "& .Mui-selected":{
+        color: "green",
+        backgroundColor: "red",
+      },
+    }
+
+
+  })(MenuItem)
 
 
 export default function WorkspaceInputDate(props) {
 
   return (
-    <div className="container" style={{ display: "flex", flexDirection:"column",}} >
+    <div className="container" style={{ display: "flex", flexDirection:"column"}} >
     <FormControl style={{marginTop:"10px"}} component="fieldset">
     <FormLabel className="MuiFormLabel-root" component="legend">Deadline</FormLabel>
       <StyledTextField
@@ -52,6 +97,20 @@ export default function WorkspaceInputDate(props) {
         fullWidth
       />
       </FormControl>
+      <StyledFormControl style={{marginTop:"10px"}}  variant="outlined">
+      <FormLabel  className="MuiFormLabel-root" component="legend">Attach Upcoming Events</FormLabel>
+        <Select
+          multiple
+          value={props.skills}
+          onChange={(e)=>props.handleUpcomingEvents(e.target.value)}
+          value={props.upcomingEvents}
+        >
+          {props.upcomingEventsList.map((event)=>
+            <MenuItem style={{color: colors.quaternaryDark}} value={event.acronym}>{event.acronym}</MenuItem>
+        )}
+
+        </Select>
+      </StyledFormControl>
       </div>
   );
 }
