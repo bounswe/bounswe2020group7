9cf61b05889bb2ad0e57a4a1a6b76ec18eba21d2
can <canbolukbas98@gmail.com>
2021-01-09 16:05:30 +0300

    A.S. forms updated

--- Platon/backend/app/activity_stream/forms.py
+++ Platon/backend/app/activity_stream/forms.py
@@ -3,10 +3,10 @@ import datetime, json
 from flask_restplus import reqparse
 
 class GetActivityStreamForm(Form):
-    page = IntegerField("page")
-    per_page = IntegerField("per_page")
+    page = IntegerField("page", validators=[validators.InputRequired()])
+    per_page = IntegerField("per_page", validators=[validators.DataRequired()])
 
 get_activity_stream_parser = reqparse.RequestParser()
-get_activity_stream_parser.add_argument('page',type=int,help="Page index that you want(Starts from 0)",location='args')
-get_activity_stream_parser.add_argument('per_page',type=int,help="Number of items in a page",location='args')
-get_activity_stream_parser.add_argument('auth_token',required=True, type=str,help="Authentication Token",location='headers')
+get_activity_stream_parser.add_argument('page', required=True, type=int, help="Page index that you want(Starts from 0)", location='args')
+get_activity_stream_parser.add_argument('per_page', required=True, type=int, help="Number of items in a page", location='args')
+get_activity_stream_parser.add_argument('auth_token',required=True, type=str, help="Authentication Token", location='headers')
