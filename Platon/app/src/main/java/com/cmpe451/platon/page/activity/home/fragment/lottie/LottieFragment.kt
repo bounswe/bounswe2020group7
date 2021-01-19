8df71995b3232bffd38fccded95b296fb08622c1
Ertugrul <ertugrul@codeway.co>
2021-01-19 14:30:43 +0300

    Logout christmas lottie animation changed with stay home lottie.

--- Platon/app/src/main/java/com/cmpe451/platon/page/activity/home/fragment/lottie/LottieFragment.kt
+++ Platon/app/src/main/java/com/cmpe451/platon/page/activity/home/fragment/lottie/LottieFragment.kt
@@ -1,37 +1,28 @@
 package com.cmpe451.platon.page.activity.home.fragment.lottie
 
-import android.animation.ValueAnimator
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
 import androidx.fragment.app.Fragment
-import com.cmpe451.platon.databinding.FragmentHomeBinding
-import com.cmpe451.platon.databinding.FragmentLottieMerryCristmasBinding
+import com.cmpe451.platon.databinding.FragmentLottieLogoutBinding
 import com.cmpe451.platon.page.activity.home.HomeActivity
-import com.cmpe451.platon.page.activity.login.LoginActivity
 
 class LottieFragment: Fragment() {
 
-    private lateinit var binding: FragmentLottieMerryCristmasBinding
+    private lateinit var binding: FragmentLottieLogoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
         (requireActivity() as HomeActivity).supportActionBar?.hide()
-
-
-
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        binding = FragmentLottieMerryCristmasBinding.inflate(inflater)
+        binding = FragmentLottieLogoutBinding.inflate(inflater)
 
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -43,8 +34,6 @@ class LottieFragment: Fragment() {
             }
 
         }
-
-
     }
 
 }
