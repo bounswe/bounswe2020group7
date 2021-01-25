b10f7e2c54bc643103a084edb0314b1e9b32a639
ahmetdadak <ahmetdadak06@gmail.com>
2021-01-26 00:21:45 +0300

    BUG FIX - Empty recommendation is resolved

--- Platon/frontend/src/components/Recommendation/WorkspaceRecommendation/WorkspaceRecommendation.js
+++ Platon/frontend/src/components/Recommendation/WorkspaceRecommendation/WorkspaceRecommendation.js
@@ -5,7 +5,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
 import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
-import Spinner from '../../Spinner/Spinner'
+import Spinner from "../../Spinner/Spinner";
 import config from "../../../utils/config";
 import colors from "../../../utils/colors";
 import axios from "axios";
@@ -15,7 +15,6 @@ const { BASE_URL } = config;
 const REC_URL = `${BASE_URL}/api/recommendation_system/workspace`;
 axios.defaults.headers.common["auth_token"] = localStorage.getItem("jwtToken");
 
-
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 400,
@@ -23,9 +22,7 @@ const useStyles = makeStyles((theme) => ({
     border: "none",
     borderRadius: "0.5em",
     backgroundColor: colors.secondary,
-
   },
-
 }));
 const NUMBER_OF_RECOMMENDATIONS = 3;
 const WorkspaceRecommendation = () => {
@@ -63,54 +60,116 @@ const WorkspaceRecommendation = () => {
 
   return (
     <div>
-        {loaded ?
-      <div className={classes.root}>
-           <Link
-                    to={`/${recommendations[activeStep].creator_id}/workspace/${recommendations[activeStep].id}`}
-                    style={{ textDecoration: "none" }}
-                  >
-          <Typography variant="h6" align="center" style={{color: colors.tertiaryDark, padding: "8px 16px"}}>{recommendations[activeStep].title}</Typography>
-          </Link>
-          <Typography style={{color: colors.tertiary, padding: "8px 16px"}}><span style={{color: colors.primary}} >Description: </span>{recommendations[activeStep].description.substring(0,120)}{recommendations[activeStep].description.length>120 ? "..."  : null}</Typography>
-          <Typography style={{color: colors.quaternaryDark, padding: "8px 16px"}}><span style={{color: colors.primary}} >Contributors: </span>{recommendations[activeStep].contributor_list.map((contributor, index) => <span>{`${contributor.name} ${contributor.surname}`}{recommendations[activeStep].contributor_list && index !== (recommendations[activeStep].contributor_list.length-1) ? " - ": null}</span>)}</Typography>
-          <Typography style={{color: colors.quinaryDark, padding: "8px 16px"}}><span style={{color: colors.primary}} >State: </span>{recommendations[activeStep].state===0 ? "Search For Collaborators" : (recommendations[activeStep].state===1 ? "Ongoing": "Published") }</Typography>
+      {loaded ? (
+        <div className={classes.root}>
+          {recommendations ? (
+            <div>
+              {recommendations.length !== 0 ?
+              <div>
+              <Link
+                to={`/${recommendations[activeStep].creator_id}/workspace/${recommendations[activeStep].id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <Typography
+                  variant="h6"
+                  align="center"
+                  style={{ color: colors.tertiaryDark, padding: "8px 16px" }}
+                >
+                  {recommendations[activeStep].title}
+                </Typography>
+              </Link>
+              <Typography
+                style={{ color: colors.tertiary, padding: "8px 16px" }}
+              >
+                <span style={{ color: colors.primary }}>Description: </span>
+                {recommendations[activeStep].description.substring(0, 120)}
+                {recommendations[activeStep].description.length > 120
+                  ? "..."
+                  : null}
+              </Typography>
+              <Typography
+                style={{ color: colors.quaternaryDark, padding: "8px 16px" }}
+              >
+                <span style={{ color: colors.primary }}>Contributors: </span>
+                {recommendations[activeStep].contributor_list.map(
+                  (contributor, index) => (
+                    <span>
+                      {`${contributor.name} ${contributor.surname}`}
+                      {recommendations[activeStep].contributor_list &&
+                      index !==
+                        recommendations[activeStep].contributor_list.length - 1
+                        ? " - "
+                        : null}
+                    </span>
+                  )
+                )}
+              </Typography>
+              <Typography
+                style={{ color: colors.quinaryDark, padding: "8px 16px" }}
+              >
+                <span style={{ color: colors.primary }}>State: </span>
+                {recommendations[activeStep].state === 0
+                  ? "Search For Collaborators"
+                  : recommendations[activeStep].state === 1
+                  ? "Ongoing"
+                  : "Published"}
+              </Typography>
 
-        <MobileStepper
-          steps={maxSteps}
-          style={{backgroundColor: colors.secondaryLight, borderRadius: "0.5em"}}
-          position="static"
-          variant="text"
-          activeStep={activeStep}
-          nextButton={
-            <Button
-              size="small"
-              onClick={handleNext}
-              disabled={activeStep === maxSteps - 1}
-            >
-              Next
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowLeft />
-              ) : (
-                <KeyboardArrowRight />
-              )}
-            </Button>
-          }
-          backButton={
-            <Button
-              size="small"
-              onClick={handleBack}
-              disabled={activeStep === 0}
-            >
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowRight />
-              ) : (
-                <KeyboardArrowLeft />
-              )}
-              Back
-            </Button>
-          }
-        />
-      </div>:<div style={{marginTop: "40px", display: "flex", flexDirection: "column", alignItems:"center"}}><Spinner/></div>}
+              <MobileStepper
+                steps={maxSteps}
+                style={{
+                  backgroundColor: colors.secondaryLight,
+                  borderRadius: "0.5em",
+                }}
+                position="static"
+                variant="text"
+                activeStep={activeStep}
+                nextButton={
+                  <Button
+                    size="small"
+                    onClick={handleNext}
+                    disabled={activeStep === maxSteps - 1}
+                  >
+                    Next
+                    {theme.direction === "rtl" ? (
+                      <KeyboardArrowLeft />
+                    ) : (
+                      <KeyboardArrowRight />
+                    )}
+                  </Button>
+                }
+                backButton={
+                  <Button
+                    size="small"
+                    onClick={handleBack}
+                    disabled={activeStep === 0}
+                  >
+                    {theme.direction === "rtl" ? (
+                      <KeyboardArrowRight />
+                    ) : (
+                      <KeyboardArrowLeft />
+                    )}
+                    Back
+                  </Button>
+                }
+              /></div>:<div>Nothing specified for you</div>}
+            </div>
+          ) : (
+            <div>Nothing to show</div>
+          )}
+        </div>
+      ) : (
+        <div
+          style={{
+            marginTop: "40px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Spinner />
+        </div>
+      )}
     </div>
   );
 };
